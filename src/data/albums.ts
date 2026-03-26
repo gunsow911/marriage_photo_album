383b5d802d96eb7ae96417d88930e4d754134150
gunsow <fukudatatsuya911@gmail.com>
2026-03-26 19:51:29 +0900

    写真を追加した

--- src/data/albums.ts
+++ src/data/albums.ts
@@ -5,8 +5,20 @@ export interface Photo {
 }
 
 export const photos: Photo[] = [
-  { id: 1, src: './photos/1.jpg', caption: "クリスマスでケンタッキーチキン" },
-  { id: 2, src: './photos/2.jpg', caption: "菜の花といっしょに" },
-  { id: 3, src: './photos/3.jpg', caption: "菜の花といっしょに2" },
-  { id: 4, src: './photos/4.jpg', caption: "結婚1か月記念" },
+  { id: 1,  src: './photos/250706-1.jpg'},
+  { id: 2,  src: './photos/250706-2.jpg'},
+  { id: 3,  src: './photos/250914-1.jpg'},
+  { id: 4,  src: './photos/250914-2.jpg'},
+  { id: 5,  src: './photos/251010.jpg'},
+  { id: 6,  src: './photos/251019.jpg'},
+  { id: 7,  src: './photos/251123-1.jpg'},
+  { id: 8,  src: './photos/251123-2.jpg'},
+  { id: 9,  src: './photos/251203.jpg'},
+  { id: 10, src: './photos/260222.jpg'},
+  { id: 11, src: './photos/260315-1.jpg'},
+  { id: 12, src: './photos/260315-2.jpg'},
+  { id: 13, src: './photos/260315-3.jpg'},
+  { id: 14, src: './photos/260322-1.jpg'},
+  { id: 15, src: './photos/260322-2.jpg'},
+  { id: 16, src: './photos/999999.jpg' },
 ]
